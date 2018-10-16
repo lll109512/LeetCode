6ed06f5d1b27b5d13e8a2f590d781053bccf3a12
Xingyu Li <lll109512@outlook.com>
2018-10-16 17:32:17 +1100

    update 240.search-a-2d-matrix-ii.python3.py

---
+++ 240.search-a-2d-matrix-ii.python3.py
@@ -0,0 +1,63 @@
+#
+# [240] Search a 2D Matrix II
+#
+# https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+#
+# algorithms
+# Medium (39.38%)
+# Total Accepted:    135.9K
+# Total Submissions: 344.5K
+# Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+#
+# Write an efficient algorithm that searches for a value in an m x n matrix.
+# This matrix has the following properties:
+# 
+# 
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+# 
+# 
+# Example:
+# 
+# Consider the following matrix:
+# 
+# 
+# [
+# ⁠ [1,   4,  7, 11, 15],
+# ⁠ [2,   5,  8, 12, 19],
+# ⁠ [3,   6,  9, 16, 22],
+# ⁠ [10, 13, 14, 17, 24],
+# ⁠ [18, 21, 23, 26, 30]
+# ]
+# 
+# 
+# Given target = 5, return true.
+# 
+# Given target = 20, return false.
+# 
+#
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        return self.helper(matrix,0,n-1,target)
+    
+    def helper(self,matrix,i,j,target):
+        if i>=len(matrix) or j < 0:
+            return False
+        if target == matrix[i][j]:
+            return True
+        else:
+            if target > matrix[i][j]:
+                return self.helper(matrix,i+1,j,target)
+            else:
+                return self.helper(matrix,i,j-1,target)
+
+        
